a8acc18d46aacbaf0967f65cba3a40b60b8d49c1
adamkr13 <adamreuter13@gmail.com>
2021-03-28 13:48:21 -0400

    Complete 3.03

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        [DisplayName("Owner Id")]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Title must be less than 20 characters.")]
+        [DisplayName("Note Title")]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
