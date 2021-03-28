6cd8232276fc29e5a83818201d759eee2ecca2dc
adamkr13 <adamreuter13@gmail.com>
2021-03-28 15:33:47 -0400

    complete 4.04

---
+++ ElevenNote.Models/Note/NoteListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models.Note
+{
+    public class NoteListItem
+    {
+        public int NoteId { get; set; }
+
+        
+        public string Title { get; set; }
+
+        [DisplayName("Date Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
---
+++ ElevenNoteMVC01/Controllers/NoteController.cs
@@ -0,0 +1,22 @@
+﻿using ElevenNote.Models.Note;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNoteMVC01.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+
+            var model = new NoteListItem[0];
+
+            return View(model);
+        }
+    }
+}
